1ef7a5d384a90b8daaa89b6eb4add97d35f917cd
Musajiji <dzhal@bk.ru>
2023-12-13 00:08:26 +0300

    -

--- task.js
+++ task.js
@@ -341,3 +341,9 @@ var maxProfit = function (prices) {
 // console.log(maxProfit([7, 1, 5, 3, 6, 4]));
 // console.log(maxProfit([7, 6, 4, 3, 1]));
 // console.log(maxProfit([2, 4, 1]));
+
+
+// 122. Best Time to Buy and Sell Stock II
+var maxProfit = function (prices) {
+
+};
